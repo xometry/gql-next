df8d149f143375cd9ba86d03346abbce78d67e3a
Luke Hendrix <lhendrix@example.com>
2020-02-27 16:36:44 -0500

    Use default_factory for client headers default

--- gql/config.py
+++ gql/config.py
@@ -12,7 +12,7 @@ class Config:
     endpoint: str
     documents: str
     custom_header: str = ''
-    client_headers: dict = {}
+    client_headers: dict = field(default_factory=list)
 
     @classmethod
     def load(cls: Type[ConfigT], filename: str) -> ConfigT:
